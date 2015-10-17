9ee4a984dc9167cedd600a696bf1d072ce179960
hank031014 <hank031014@gmail.com>
2015-10-17 17:13:45 +0800

    Drawer簡單版

--- test/app/src/main/java/com/example/mac/test/MainActivity.java
+++ test/app/src/main/java/com/example/mac/test/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.mac.test;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static com.example.mac.test.R.drawable.ic_drawer;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment f = new FragmentA();
+        fm.beginTransaction().replace(R.id.content, f).commit();
     }
 }
