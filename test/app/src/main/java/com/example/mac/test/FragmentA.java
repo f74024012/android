954384cb5e7fac6651854ffb892147a555e0c5cc
f74024012 <james22664505@gmail.com>
2015-10-17 19:59:40 +0800

    add picture

--- test/app/src/main/java/com/example/mac/test/FragmentA.java
+++ test/app/src/main/java/com/example/mac/test/FragmentA.java
@@ -1,15 +1,19 @@
 package com.example.mac.test;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class FragmentA extends Fragment {
+    ImageView iv;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,7 +25,13 @@ public class FragmentA extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        View view = inflater.inflate(R.layout.fragment_a, container, false);
+
+
+        iv=(ImageView) view.findViewById(R.id.iv);
+        Bitmap bm= BitmapFactory.decodeResource(getResources(), R.drawable.cat);
+        iv.setImageBitmap(bm);
+        return view;
     }
 
 }
